f4cc42543e34c13e95f15c00ba000c4ab1243937
Robert Rozic <r.rozic97@gmail.com>
2014-10-07 23:39:15 +0200

    SELinux: Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
