cdd82ce313ebb06fe65b510cd5a62b486f5b3590
Robert Rozic <r.rozic97@gmail.com>
2014-10-12 02:31:15 +0200

    Wifi: Fix wlan binary symlink -Clean qcom.wifi script -Update ueventd permissions

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -50,7 +50,7 @@ LOCAL_MODULE := WCNSS_qcom_wlan_factory_nv.bin
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): TARGET := /persist/$(LOCAL_MODULE)
+$(LOCAL_BUILT_MODULE): TARGET := /efs/wifi/.mac.info
 $(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT)/etc/firmware/wlan/prima/$(LOCAL_MODULE)
 $(LOCAL_BUILT_MODULE):
 	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
