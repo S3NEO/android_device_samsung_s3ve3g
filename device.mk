606bd6c71625365602b01ec2ec181fcca0edc13a
Robert Rozic <r.rozic97@gmail.com>
2014-10-13 20:23:51 +0200

    RIL: Add ATFWD-daemon service
    +Copy Wifi firmware files from kernel
    +Vold fixes

--- device.mk
+++ device.mk
@@ -273,8 +273,8 @@ PRODUCT_PACKAGES += \
     wcnss_service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    kernel/samsung/s3ve3g/drivers/staging/prima/firmware_bin/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    kernel/samsung/s3ve3g/drivers/staging/prima/firmware_bin/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 PRODUCT_PACKAGES += \
