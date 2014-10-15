3dc8ab1ce75a08a0f48a58f9ec00e00a7041419a
Robert Rozic <r.rozic97@gmail.com>
2014-10-15 19:40:37 +0200

    audio: Symlink wcd9320 to 9306
    Only wcd9320 is in data/misc/audio
    It is symlinked like that on stock

--- Android.mk
+++ Android.mk
@@ -216,7 +216,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(WV_SYMLINKS)
 
 # Create links for audcal data files
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9306; \
-    ln -sf /data/misc/audio/wcd9306_anc.bin \
+    ln -sf /data/misc/audio/wcd9320_anc.bin \
     $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_anc.bin; \
     ln -sf /data/misc/audio/mbhc.bin \
     $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_mbhc.bin)
