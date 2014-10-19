d91afad5532e99367ef702c84ebf000267eb47a5
Robert Rozic <r.rozic97@gmail.com>
2014-10-19 02:18:23 +0200

    s3ve3g: Add missing blobs for GPS and sensors

--- device.mk
+++ device.mk
@@ -113,8 +113,9 @@ PRODUCT_PACKAGES += \
     gps.msm8226
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf \
 
 # IRSC
 PRODUCT_COPY_FILES += \
