18f31f89dc74d1e84bdeb47c5bce797669d69bf0
Robert Rozic <r.rozic97@gmail.com>
2014-12-06 15:34:07 +0100

    s3ve3g: Tune power management and rename defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/s3ve3g
 TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_s3ve3g_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_s3ve3g_defconfig
 
 WLAN_MODULES:
 	mkdir -p $(KERNEL_MODULES_OUT)/pronto
