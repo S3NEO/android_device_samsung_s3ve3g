45b761ccb833708eedc1aabcf1809e04fb7013f9
Robert Rozic <r.rozic97@gmail.com>
2014-12-16 17:41:00 +0100

    Camera: Fix exposure
    +Make torch brighter

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -113,7 +113,9 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     // fix params here
     params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
-
+    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.5");
+    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-4");
+    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "4");
     /* If the vendor has HFR values but doesn't also expose that
      * this can be turned off, fixup the params to tell the Camera
      * that it really is okay to turn it off.
