bf1fb029ebc2bdc8d77ebbcaca9d7c13cf30d60c
Rox- <r.rozic97@gmail.com>
2015-01-07 12:31:47 +0100

    s3ve3g: Update bluetooth config for L

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,11 +21,11 @@
 #define BTM_DEF_LOCAL_NAME   "Galaxy S3 Neo"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
-#define BLUETOOTH_QCOM_SW TRUE
-#define BTA_HOST_INTERLEAVE_SEARCH TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD TRUE
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS 16
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD FALSE
 
 #endif
