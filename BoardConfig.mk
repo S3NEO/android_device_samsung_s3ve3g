254001e2adcac19775691bf066ba4ee3514e6168
Rox- <r.rozic97@gmail.com>
2015-01-10 00:17:05 +0100

    s3ve3g: Set bootdevice flag
    *Add bootdevice parameter to kernel command line to indicate the
    boot storage device (msm_sdcc.1).
    *Suppress userspace undefined instructions logs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_UNIFIED_DEVICE := true
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x37 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
