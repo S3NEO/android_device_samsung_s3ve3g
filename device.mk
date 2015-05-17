86525948f0d001f2197213e7f8a0f4f906f4ea9a
Rox- <r.rozic97@gmail.com>
2015-05-17 13:40:31 +0200

    AmbientDisplay: Enable Ambient Display for s3ve3g

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8226
