5cb549ce195963fe69e492e6bc819fa567883bde
Rox- <r.rozic97@gmail.com>
2015-07-13 20:40:57 +0200

    s3ve3g: Unified defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/s3ve3g
-TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_s3ve3g_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_s3ve3g_defconfig
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
