f256ae18bac8922778c50c4464f772b61fcf363a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-12-04 13:51:17 +0800

    s3ve3g: Move GSM properties to system prop file
    Change-Id: If9090a7cede1a97099386ee4632abc58769e8499

--- init/init_s3ve3g.cpp
+++ init/init_s3ve3g.cpp
@@ -36,12 +36,6 @@
 
 #include "init_msm.h"
 
-void gsm_properties()
-{
-    property_set("telephony.lteOnGsmDevice", "0");
-    property_set("ro.telephony.default_network", "0");
-}
-
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
     char platform[PROP_VALUE_MAX];
@@ -66,14 +60,12 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "s3ve3gxx-user 4.4.2 KOT49H I9301IXXUANL1 release-keys");
         property_set("ro.product.model", "GT-I9301I");
         property_set("ro.product.device", "s3ve3g");
-        gsm_properties();
     } else if (strstr(bootloader, "I9301Q")) {
         /* s3ve3gjv */
         property_set("ro.build.fingerprint", "samsung/s3ve3gjv/s3ve3g:4.4.2/KOT49H/I9301QXXUANH1:user/release-keys");
         property_set("ro.build.description", "s3ve3gjv-user 4.4.2 KOT49H I9301QXXUANH1 release-keys");
         property_set("ro.product.model", "GT-I9301Q");
         property_set("ro.product.device", "s3ve3gjv");
-        gsm_properties();
     } else if (strstr(bootloader, "I9300I")) {
         /* s3ve3gds */
         property_set("ro.build.fingerprint", "samsung/s3ve3gdsxx/s3ve3gds:4.4.4/KTU84P/I9300IXWUBNJ1:user/release-keys");
@@ -83,7 +75,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.multisim.set_audio_params", "true");
         property_set("ro.multisim.simslotcount", "2");
         property_set("persist.radio.multisim.config", "dsds");
-        gsm_properties();
     }
 
     property_get("ro.product.device", device);
