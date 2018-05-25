c35a531119a159fb058685f3a82fc8e4e6c752bc
leonardo meitz <leonardomeitz@gmail.com>
2018-05-25 12:57:59 +0200

    audio fix derp paths

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/samsung/s3ve3g/s3ve3g-vendor.mk)
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Doze
