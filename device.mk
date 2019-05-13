09de00d6993780e2ae97c8743db19bc03993e08f
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-13 22:01:49 +0200

    s3ve3g: import media_codecs_performance from LA.BF.1.1.3_rb1.15

--- device.mk
+++ device.mk
@@ -52,7 +52,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
     
 # NFC
 $(call inherit-product, device/samsung/s3ve3g/nfc/pn547/product.mk)    
