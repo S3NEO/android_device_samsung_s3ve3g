b688b3b47140fab3483d5b7811135d4573c5a940
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-13 20:18:00 +0200

    s3ve3g: import sensors MultiHAL

--- device.mk
+++ device.mk
@@ -66,6 +66,13 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc   
     
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8226
+    
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/configs/sensors/_hals.conf::$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
+    
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8226.conf
